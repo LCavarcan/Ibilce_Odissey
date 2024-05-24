2d99d09da53bd40cfb1c0a044bad16004a037267
LCavarcan <gluizacavarcan@gmail.com>
2024-05-24 10:46:08 -0300

    Fiz as missoes aparecerem na tela

--- Ibilce_Odissey/componentes/ACME.js
+++ Ibilce_Odissey/componentes/ACME.js
@@ -1,6 +1,6 @@
-import { Button } from "@rneui/themed";
-import React from "react";
-import { View, Text } from "react-native";
+import { Button, Dialog, ListItem } from "@rneui/themed";
+import React, {useState} from "react";
+import { View, Text, FlatList } from "react-native";
 import Estilo from "./Estilo";
 import { useNavigation } from '@react-navigation/native';
 import { Header, Icon } from "@rneui/base";
@@ -8,16 +8,50 @@ import { Header, Icon } from "@rneui/base";
 export default function Inicio() {
   const navigation = useNavigation();
 
-  const missoes = ['Ache o Switch', 'Conecte o Servidor']
+  const missoes = [
+    {id: 1, name: 'Ache o Switch'}, 
+    {id: 2, name: 'Conecte o Servidor'}
+  ]
+
+  const [visible, setVisible] = useState(false);
+
+  const renderItem = ({ item }) => (
+    <View style={{ padding: 10 }}>
+      <Text>{item.name}</Text>
+    </View>
+  );
 
   return (
     <View style={{ alignItems: "center" }}>
       <Header
         backgroundColor="white"
         // Vai levar para as telas das missões ou mapa
-        leftComponent={{icon: 'question-mark'}}
+        leftComponent={{
+          icon: 'question-mark', 
+          onPress:(() => {
+            setVisible(true) 
+          })}
+        }
         rightComponent={{icon: 'navigation'}}
       />
+      <Dialog
+        isVisible={visible}
+      >
+        <Dialog.Title title="Missões"/>
+        <Dialog.Actions>
+          <FlatList
+            data={missoes}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
+        </Dialog.Actions>
+        <Dialog.Actions>
+          <Dialog.Button
+            title="Fechar Tela"
+            onPress={() => setVisible(false)}
+          />
+        </Dialog.Actions>
+      </Dialog>
     </View>
   );
 }
