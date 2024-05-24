7a538cbab2b92a61a3ba7ab43b6985e8e7249e3d
Ana Catarina Marques Vicentim <catarinaanamarques830@gmail.com>
2024-05-24 20:31:55 -0300

    Update MyStack.js

--- Ibilce_Odissey/componentes/MyStack.js
+++ Ibilce_Odissey/componentes/MyStack.js
@@ -3,7 +3,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Inicio from './Inicio';
 import Criar from './Criar';
 import ACME from './ACME'
-
+import NewUser from './newUser';
+import ReplacePass from './replacePass';
+import Login from './login';
 const Stack = createNativeStackNavigator()
 
 export default function MyStack() {
@@ -11,6 +13,9 @@ export default function MyStack() {
         <Stack.Navigator initialRouteName='Inicio'>
             {/* Definição das telas que serão usadas */}
             <Stack.Screen name='Inicio' component={Inicio}/>
+            <Stack.Screen name='Login' component={Login}/>
+            <Stack.Screen name='ReplacePass' component={ReplacePass}/>
+            <Stack.Screen name='NewUser' component={NewUser}/>
             <Stack.Screen name='Criar' component={Criar}/>
             <Stack.Screen name='ACME' component={ACME}/>
         </Stack.Navigator>
